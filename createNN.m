bee1d2d2c82da433e2f85f52b80d2b89ef5a3d33
HelloThereGK <49295566+HelloThereGK@users.noreply.github.com>
2021-07-24 10:45:02 -0400

    normalized

--- createNN.m
+++ createNN.m
@@ -10,11 +10,16 @@ endTime = allData.End_Time;
 duration = endTime - startTime;
 duration = duration';
 duration = datenum(duration);
+duration = normalize(duration);
 
 temperature = allData.Temperature_F_';
+temperature = normalize(temperature);
 humidity = allData.Humidity___';
+humidity = normalize(humidity);
 visibility = allData.Visibility_mi_';
+visibility = normalize(visibility);
 windSpeed = allData.Wind_Speed_mph_';
+windSpeed = normalize(windSpeed);
 crossing = allData.Crossing';
 crossing = double(crossing);
 mStop = allData.Stop';
@@ -25,12 +30,14 @@ trafficSignal = double(trafficSignal);
 % ordinal encoding for weather
 weatherCond = allData.Weather_Condition';
 weatherCond = categorical(weatherCond);
-weatherCondOrd = grp2idx(weatherCond)'; % use unique to find how it has been transcribed
+weatherCondOrd = grp2idx(weatherCond)';
+weatherCondOrd = normalize(weatherCondOrd);% use unique to find how it has been transcribed
 
 dayNight = allData.Sunrise_Sunset';
 dayNight = categorical(dayNight);
 dayNightOrd = grp2idx(dayNight)';
 dayNightOrd = dayNightOrd - ones(1, 50000);
+dayNightOrd = normalize(dayNightOrd);
 
 Predictors = [duration; temperature; humidity; visibility; windSpeed; ...
     crossing; mStop; trafficSignal; weatherCondOrd; dayNightOrd]';
