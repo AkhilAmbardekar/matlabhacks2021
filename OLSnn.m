4fe2693ca0c1e13f3b70d91892f4ae2ed6e2ce3e
HelloThereGK <49295566+HelloThereGK@users.noreply.github.com>
2021-07-24 18:46:18 -0400

    updated inputs

--- OLSnn.m
+++ OLSnn.m
@@ -10,3 +10,9 @@ allData = readtable("cancer_reg.csv");
 mortRate = allData.TARGET_deathRate;
 
 %extract inputs
+medIncome = allData.medIncome;
+popEst2015 = allData.popEst2015;
+povertyPercent = allData.povertyPercent;
+studyPerCap = allData.studyPerCap;
+medAge = allData.MedianAge;
+avgHouse = allData.AvgHouseholdSize;
